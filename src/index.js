ce809686f490ac75c56ec04437ba2edcdf1ebee0
Ulferno <36534987+Ulferno@users.noreply.github.com>
2021-11-08 22:36:19 +0000

    added validation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const fs = require('fs');
+const path = require('path');
+
 try {
+	const apiKey = core.getInput('api-key');
+	const experienceId = core.getInput('experienceId');
+	const filePath = core.getInput('file');
+	const shouldPublish = core.getInput('shouldPublish');
+
+	const validPath = fs.existsSync(filePath);
+
+	if (!validPath) {
+		return core.setFailed('Invalid file path');
+	}
+
+	console.log(filePath)
+	console.log(path.basename(filePath), path.extname(filePath))
+
 	core.setOutput("version", 1);
 
 	const payload = JSON.stringify(github.context.payload, undefined, 2)
